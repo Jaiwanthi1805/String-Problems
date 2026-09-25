e37ed0a36738f388040802baecd260109645a6f7
Jaiwanthi <jaiwanthi18@gmail.com>
2026-09-25 18:11:17 +0530

    commit

---
+++ p27.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static boolean isAnagram(String a, String b) {
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            count[a.charAt(i)-'a']++;
+            count[b.charAt(i)-'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String a = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String b = sc.nextLine();
+
+        System.out.println(isAnagram(a, b));
+    }
+}
